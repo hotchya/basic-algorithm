0120683c9185a19e44170f4b9dc92f6aa2a7a01f
hotchya <54389889+hotchya@users.noreply.github.com>
2021-03-05 13:17:43 +0900

    Update break_1_byte.py

--- symmetric key encryption/break_1_byte.py
+++ symmetric key encryption/break_1_byte.py
@@ -1,4 +1,4 @@
-# You are a hecker. You have data that is encrypted by stealing the transmitted data, and you know that this data is in plain text.
+# You are a hacker. You have data that is encrypted by stealing the transmitted data, and you know that this data is in plain text.
 # You need to find out the original data and the secret key (8bits).
 
 data = '6e5253491a5e5b4e5b1a53491a5b5649551a4c5f48431a53574a55484e5b544e1a4e5f424e1a5e5b4e5b141a6a565f5b495f1a4e5b515f1a595b485f1a4d525f541a525b545e5653545d14'
